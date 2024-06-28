9c1cc8d6d90e8b5ebdbdc00dbebaaaf85cd8e841
amCtrld <petrembugua@proton.me>
2024-06-29 02:20:24 +0300

    Darkmode | Switch between Dark and Light mode

--- Projects/Color Flipper/index.js
+++ Projects/Color Flipper/index.js
@@ -1,11 +1,20 @@
-const changeColorButton = document.getElementById('changeColor');
+const changeColorButton = document.getElementById("changeColor");
+const iconSpan = document.getElementById("icon");
 
 let currentColorIndex = 0;
 
-const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'black'];
+const colors = ["white", "black"];
 
-changeColorButton.addEventListener('click', () => {
+changeColorButton.addEventListener("click", () => {
+  currentColorIndex = (currentColorIndex + 1) % colors.length;
   const color = colors[currentColorIndex];
   document.body.style.backgroundColor = color;
-  currentColorIndex = (currentColorIndex + 1) % colors.length;
+
+  if (colors[currentColorIndex] === "white") {
+    iconSpan.innerHTML = '<i class="material-icons">brightness_2</i>'; // Moon icon
+    document.body.style.color = "black";
+  } else {
+    iconSpan.innerHTML = '<i class="material-icons">brightness_high</i>'; // Sun icon
+    document.body.style.color = "white";
+  }
 });
