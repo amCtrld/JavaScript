91d4ffeec513e55980b54ffbc5e88f019334cf68
amCtrld <petrembugua@proton.me>
2024-06-29 03:03:12 +0300

    Total Fix | Completely fixed the DOm rendering issue where icons were not loading

--- Projects/Color Flipper/index.js
+++ Projects/Color Flipper/index.js
@@ -1,15 +1,16 @@
 const changeColorButton = document.createElement('button');
-changeColorButton.classList.add('w3-button', 'w3-blue');
+changeColorButton.classList.add('w3-button', 'w3-border', 'w3-round-large');
 changeColorButton.type = "button";
 changeColorButton.id = "changeColorButton";
 
 const iconElement = document.createElement('i');
 iconElement.classList.add('material-icons');
+iconElement.textContent = 'brightness_2';
 changeColorButton.appendChild(iconElement);
 
 const paragraph = document.createElement('p');
 paragraph.textContent = 'Click on the button to switch theme';
-paragraph.classList.add('w3-center', 'w3-large', 'w3-text-blue');
+paragraph.classList.add('w3-center', 'w3-large');
 
 const container = document.createElement('div');
 container.classList.add('w3-container', 'w3-center', 'w3-margin-top', 'w3-display-middle');
